7223520441cf52a3e1170a6109d96858fd9089ab
currently-coding <165482223+currently-coding@users.noreply.github.com>
2025-01-03 09:41:13 +0100

    improved particle animation by adding comparison

--- video1/src/intro.py
+++ video1/src/intro.py
@@ -3,7 +3,7 @@ import manim_physics as p
 
 
 class MagneticFieldExmaple(m.Scene):
-    def construct(self):
+    def bar_magnet_field_lines(self):
         # Load the image (use the image file name or full path)
         image = m.ImageMobject(
             "../bar-magnet-icon-n-pole-and-s-pole-magnets-png-3540789338.png"
@@ -17,89 +17,39 @@ class MagneticFieldExmaple(m.Scene):
         image.to_edge(m.UP)  # Moves the image to the top edge of the screen
         image.shift(m.UP * 0.5)
 
+        north = (
+            m.Rectangle(m.BLACK, height=0.8, width=1.5)
+            .shift(m.LEFT * 0.5)
+            .set_fill(m.GREEN, opacity=100)
+        )
+        south = m.Rectangle(m.BLACK, height=0.8, width=1.5).set_fill(m.RED, opacity=100)
+        south.next_to(north, buff=0)
+
         # Adding the field lines
-        wire = p.Wire(m.Circle(1).rotate(m.PI / 2, m.UP), current=4)
+        wire1 = p.Wire(m.Circle(1).rotate(m.PI / 2, m.UP), current=2).shift(
+            m.RIGHT * 0.41
+        )
+        wire5 = p.Wire(m.Circle(1).rotate(m.PI / 2, m.UP), current=2).shift(
+            m.RIGHT * 0.2
+        )
+        wire2 = p.Wire(m.Circle(1).rotate(m.PI / 2, m.UP), current=2).shift(
+            m.LEFT * 0.41
+        )
+        wire4 = p.Wire(m.Circle(1).rotate(m.PI / 2, m.UP), current=2).shift(
+            m.LEFT * 0.2
+        )
+        wire3 = p.Wire(m.Circle(1).rotate(m.PI / 2, m.UP), current=2)
+        wires = [wire1, wire2, wire3, wire4, wire5]
         mag_field = p.MagneticField(
-            wire,
+            *wires,
             x_range=[-10, 10],
             y_range=[-10, 10],
         )
         self.add(mag_field)
         # Add the image to the scene
         self.add(image)
+        self.add(south, north)
+        return (image, mag_field)
 
-        # Hold the image on screen for 3 seconds
-        self.wait(3)
-
-
-class AttractionRepulsion(m.Scene):
     def construct(self):
-        # Create the first wire and position it to the left
-        wire1 = p.Wire(m.Circle(radius=1).rotate(m.PI / 2, m.UP), current=4).shift(
-            m.LEFT * 3
-        )
-
-        # Create the second wire and position it to the right
-        wire2 = p.Wire(m.Circle(radius=1).rotate(m.PI / 2, m.UP), current=4).shift(
-            m.RIGHT * 3
-        )
-
-        # Create the first image and set its properties
-        img1 = m.ImageMobject(
-            "../bar-magnet-icon-n-pole-and-s-pole-magnets-png-3540789338.png"
-        )
-        img1.rotate(-m.PI / 4)
-        img1.scale(0.4)
-        img1.to_edge(m.UP).shift(m.UP * 0.5 + m.RIGHT * 3)
-
-        # Create the second image and set its properties
-        img2 = m.ImageMobject(
-            "../bar-magnet-icon-n-pole-and-s-pole-magnets-png-3540789338.png"
-        )
-        img2.rotate(-m.PI / 4)
-        img2.scale(0.4)
-        img2.to_edge(m.UP).shift(m.UP * 0.5 + m.LEFT * 3)
-
-        # Create the magnetic field for the two wires
-        magField = p.MagneticField(
-            wire1,
-            wire2,
-            x_range=[-10, 10],
-            y_range=[-10, 10],
-        )
-
-        # Add elements to the scene
-        self.add(magField, img1, img2)
-
-        # Define an updater function to update the magnetic field dynamically
-        def update_field(mob):
-            mob.become(
-                p.MagneticField(
-                    wire1,
-                    wire2,
-                    x_range=[-10, 10],
-                    y_range=[-10, 10],
-                )
-            )
-
-        magField.add_updater(update_field)
-
-        # Animate the 180-degree rotation of img1 and wire2 simultaneously
-        self.play(
-            m.AnimationGroup(
-                img1.animate.rotate(m.PI / 2),
-                wire2.animate.rotate(m.PI / 2),
-                lag_ratio=0,
-            ),
-        )
-        self.wait(3)
-        self.play(
-            m.AnimationGroup(
-                img1.animate.rotate(m.PI / 2),
-                wire2.animate.rotate(m.PI / 2),
-                lag_ratio=0,
-            )
-        )
-
-        # Wait to display the final state
-        self.wait(5)
+        self.add(*self.bar_magnet_field_lines())
