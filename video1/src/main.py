116ade661b82b3d90b8c3baa96dc1a89b963bda7
currently-coding <165482223+currently-coding@users.noreply.github.com>
2025-01-09 08:06:09 +0100

    added picture with arrows and small fixes

--- video1/src/main.py
+++ video1/src/main.py
@@ -1,5 +1,7 @@
 from dataclasses import field
-from manim_physics import Wire, MagneticField
+from os import wait
+from manim.typing import Image
+from manim_physics import ImageMobjectFromCamera, Wire, MagneticField
 from manim import (
     TracedPath,
     Rectangle,
@@ -7,17 +9,10 @@ from manim import (
     GREEN,
     RED,
     BLUE,
-    ORANGE,
-    TEAL,
-    PURPLE,
-    MAROON,
     ImageMobject,
-    Line,
     Scene,
-    Square,
     Text,
     MathTex,
-    WHITE,
     ThreeDScene,
     VGroup,
     Write,
@@ -36,18 +31,13 @@ from manim import (
     UL,
     DL,
     FadeIn,
-    UR,
     ORIGIN,
-    TransformMatchingTex,
     Arrow,
-    Create,
     Transform,
     Dot,
-    VMobject,
     MoveAlongPath,
     DEGREES,
     YELLOW,
-    ArrowSquareTip,
     linear,
     Circle,
     PI,
@@ -87,7 +77,7 @@ class LorentzKraftVideo(Scene):
         for tex, color in [
             (r"\vec{v}", GREEN),
             (r"\vec{B}", BLUE),
-            (r"\vec{F}_L", RED),
+            (r"\vec{F}_L", YELLOW),
         ]:
             lorentz_eqn.set_color_by_tex(tex, color)
 
@@ -103,6 +93,8 @@ class LorentzKraftVideo(Scene):
         # TODO: Hier Probemagnet hinzufuegen und hervorheben
         #
         # Hier lorentz_particle_comparison() zeigen und definieren
+        self.particle_deflection(velocity=2)
+        self.wait(0.5)
         self.particle_deflection()
         self.play(Write(definition_group))
         self.wait(5)
@@ -111,10 +103,6 @@ class LorentzKraftVideo(Scene):
         # TODO: Hier Lorentz zeigen(Bild von ihm)
         #
         # Hier Formel zeigen
-        self.play(
-            lorentz_eqn.animate.to_edge(UR),
-        )
-        #
         # Hier Kreuzprodukt aufloesen
         #
         # Hier beziehungen zeigen
@@ -129,7 +117,7 @@ class LorentzKraftVideo(Scene):
         for tex, color in [
             (r"\vec{v}", GREEN),
             (r"\vec{B}", BLUE),
-            (r"\vec{F}_L", RED),
+            (r"\vec{F}_L", YELLOW),
         ]:
             eqn.set_color_by_tex(tex, color)
 
@@ -144,7 +132,7 @@ class LorentzKraftVideo(Scene):
         )  # Reduced buffer to move everything up
 
         # Color coding for equation
-        for tex, color in [(r"v", GREEN), (r"B", BLUE), (r"F_L", RED)]:
+        for tex, color in [(r"v", GREEN), (r"B", BLUE), (r"F_L", YELLOW)]:
             eqn1.set_color_by_tex(tex, color)
 
         # Create and position text elements relative to equation
@@ -180,10 +168,10 @@ class LorentzKraftVideo(Scene):
 
         self.play(FadeOut(eqn), FadeOut(text_group))
 
-        self.lorentz_particle()
-        self.lorentz_particle(B_strength=2)
         self.lorentz_particle(velocity=1)
-        self.lorentz_particle(charge=1.5)
+        self.lorentz_particle(velocity=2)
+        self.lorentz_particle(charge=1)
+        self.lorentz_particle(charge=2)
         #
         # Hier 3-Finger Regel zeigen
         #
@@ -199,12 +187,12 @@ class LorentzKraftVideo(Scene):
             .set_color(BLUE)
         )
 
-        F_vector = Vector(2.5 * LEFT + 1 * DOWN).set_color(RED)
+        F_vector = Vector(2.5 * LEFT + 1 * DOWN).set_color(YELLOW)
         F_label = (
             Tex(r"$\vec{F}_L$")
             .move_to(F_vector.get_end())
             .shift(DL * 0.5)
-            .set_color(RED)
+            .set_color(YELLOW)
         )
 
         self.play(Write(hand))
@@ -225,35 +213,9 @@ class LorentzKraftVideo(Scene):
                 F_vector,
             )
         )
-        # TODO: Hier Versuch mit Pfeilen zeigen
-        #
-        # Fertig!
-        # (evtl code scroll down zeigen)
-        # ----- Ende -----
-
-        # Animation Sequenz
-        #
-        #
-        # self.play(Write(title_group))
-        # self.wait(2)
-        # self.play(FadeOut(title_group))
-
-        # self.play(Write(experiment_note))
-        # self.wait(2)
-        # self.play(FadeOut(experiment_note))
-
         self.wait(3)
-
-        # Right-hand rule demonstration
-
-        # Application placeholder
-        application_text = Text("Anwendung auf den Versuch", font_size=36).move_to(
-            ORIGIN
-        )
-
-        self.play(Write(application_text))
-        self.wait(2)
-        self.play(FadeOut(application_text))
+        self.explain_picture()
+        self.wait(5)
 
     def bar_magnet_field_lines(self):
         # Load the image (use the image file name or full path)
@@ -263,11 +225,10 @@ class LorentzKraftVideo(Scene):
         image.rotate(-1 * (PI / 4))
 
         # Resize the image (optional)
-        image.scale(0.4)  # Adjust the scale factor as needed
+        image.scale(0.47)  # Adjust the scale factor as needed
 
         # Position the image (optional)
-        image.to_edge(UP)  # Moves the image to the top edge of the screen
-        image.shift(UP * 0.5)
+        image.move_to(ORIGIN)
 
         north = (
             Rectangle(BLACK, height=0.8, width=1.5)
@@ -278,18 +239,18 @@ class LorentzKraftVideo(Scene):
         south.next_to(north, buff=0)
 
         # Adding the field lines
-        wire3 = Wire(Circle(1).rotate(PI / 2, UP), current=2)
+        wire3 = Wire(Circle(1).rotate(PI / 2, UP), current=7).shift(LEFT * 0.0001)
         mag_field = MagneticField(
             wire3,
             x_range=[-10, 10],
             y_range=[-10, 10],
         )
+        self.add(mag_field)
         self.add(image)
-        self.play(FadeIn(mag_field))
         # Add the image to the scene
         # self.add(south, north)
         self.wait(3)
-        self.play(FadeOut(mag_field, image))
+        self.remove(mag_field, image)
 
     def lorentz_particle(
         self, charge=1.0, velocity=2.0, B_strength=1.2, angle_degrees=45
@@ -346,7 +307,7 @@ class LorentzKraftVideo(Scene):
             .shift(RIGHT * radius)
         )
         center = particle
-        center_offset = ORIGIN # fixed a so-called bug utilizing advanced linear algebra and vector calculus
+        center_offset = ORIGIN  # fixed a so-called bug utilizing advanced linear algebra and vector calculus
 
         # Time for exactly one rotation
         omega = velocity / radius  # Angular frequency
@@ -370,7 +331,7 @@ class LorentzKraftVideo(Scene):
             "max_tip_length_to_length_ratio": 0.4,
         }
 
-        force_vector = Arrow(start=ORIGIN, end=RIGHT, color=RED, **arrow_kwargs)
+        force_vector = Arrow(start=ORIGIN, end=RIGHT, color=YELLOW, **arrow_kwargs)
 
         velocity_vector = Arrow(start=ORIGIN, end=RIGHT, color=GREEN, **arrow_kwargs)
 
@@ -436,165 +397,24 @@ class LorentzKraftVideo(Scene):
             ]
         )
 
-    def lorentz_particle_comparison(
-        self, charge=1.0, velocity=2.0, B_strength=1.2, angle_degrees=45
-    ):
-        # -------- PARAMETERS (adjust here) --------
-
-        # -------- SETUP --------
-        angle = angle_degrees * DEGREES  # Convert to radians
-        force = charge * velocity * B_strength  # * sin(90)
-
-        # -------- MAGNETIC FIELD --------
-        B_field = (
-            VGroup(*[Text("×", font_size=24) for _ in range(30)])
-            .arrange_in_grid(5, 6, buff=0.7)
-            .shift(RIGHT * 2)
-        )
-        B_field.set_color(BLUE)
-
-        # -------- PARTICLE --------
-        # needs to be right by one radius of circle
-        particle = Dot(color=RED if charge > 0 else BLUE).center()
-
-        # -------- LABELS --------
-        B_label = (
-            Tex(r"$\vec{B}$").set_color(BLUE).next_to(B_field, UP, buff=0.2)
-        )  # B vector above field
-        q_label = Tex(f"q = {charge} e").set_color(RED if charge > 0 else BLUE)
-        v_label = Tex(r"$v = " + str(velocity) + r"\,\mathrm{\frac{m}{s}}$").set_color(
-            GREEN
-        )
-
-        # Label positioning
-        q_label.to_corner(UL)
-        v_label.next_to(q_label, DOWN)
-
-        def get_B_field_bounds(B_field):
-            """Get the bounds (left, right, top, bottom) of the B_field."""
-            left = B_field.get_left()[0]  # x-coordinate of the leftmost point
-            right = B_field.get_right()[0]  # x-coordinate of the rightmost point
-            top = B_field.get_top()[1]  # y-coordinate of the topmost point
-            bottom = B_field.get_bottom()[1]  # y-coordinate of the bottommost point
-            return left, right, top, bottom
-
-        field_bounds = get_B_field_bounds(B_field)
-        field_center = np.array(
-            [
-                abs(field_bounds[1] + field_bounds[0]) / 2,
-                abs(field_bounds[2] + field_bounds[3]) / 2,
-                0,
-            ]
-        )
-        # spawns center of b_field
-        radius = abs(velocity * charge / B_strength)
-        circle = Circle(radius=radius, color=YELLOW).move_to(field_center)
-        # circle.rotate(-angle, about_point=center_offset)
-        circle.set_stroke(opacity=0.3)
-
-        # -------- TRAJECTORY CALCULATION --------
-        # circle center needs to be in field
-        center_offset = field_center + radius * np.array(
-            [-np.sin(angle), np.cos(angle), 0]
-        )
-        particle.move_to(field_center).shift(RIGHT * radius)
-        # particle2.move_to(-field_center)
-
-        # Time for exactly one rotation
-        omega = velocity / radius  # Angular frequency
-        t_max = 2 * np.pi / omega  # Time for one full rotation
-
-        # -------- FORCE VECTOR --------
-        # Scaling factors
-        force_scale = 0.5
-        velocity_scale = 0.5
-
-        # Standard values for all arrows
-        arrow_kwargs = {
-            "tip_length": 0.4,  # Same tip size for all arrows
-            "max_tip_length_to_length_ratio": 0.4,
-        }
-
-        force_vector = Arrow(start=ORIGIN, end=RIGHT, color=RED, **arrow_kwargs)
-
-        velocity_vector = Arrow(start=ORIGIN, end=RIGHT, color=GREEN, **arrow_kwargs)
-
-        def update_force_vector(arrow):
-            # Calculate new start and end points
-            start = particle.get_center()
-            direction = center_offset - start
-            direction = direction / np.linalg.norm(direction) * force * force_scale
-            # Update existing arrow
-            arrow.put_start_and_end_on(start, start + direction)
-
-        def update_velocity_vector(arrow):
-            # Calculate new start and end points
-            start = particle.get_center()
-            # Vector from circle center to particle
-            radial = start - center_offset
-            # Tangential vector (90° rotation)
-            tangent = np.array([-radial[1], radial[0], 0])
-            # Normalize and scale
-            tangent = tangent / np.linalg.norm(tangent) * velocity * velocity_scale
-            # Update existing arrow
-            arrow.put_start_and_end_on(start, start + tangent)
-
-        # Füge den Updater hinzu
-        force_vector.add_updater(update_force_vector)
-        velocity_vector.add_updater(update_velocity_vector)
-        circle_circumference = radius * 2 * PI
-
-        particle2 = (
-            Dot(color=RED if charge > 0 else BLUE)
-            .shift((circle_circumference / 4) * DOWN)
-            .shift(LEFT * 4)
-        )
-        end2 = Dot().shift(circle_circumference / 2 * UP).shift(LEFT * 4)
-        rate = lambda t: min(max(3 * t - 1, 0), 1)
-        path2 = Line(start=particle2.get_center(), end=end2.get_center())
-
-        # -------- ANIMATION --------
-
-        self.play(
-            FadeIn(particle2),
-            FadeIn(B_field),
-            FadeIn(particle),
-            FadeIn(velocity_vector, rate_func=rate),
-        )
+    def explain_picture(self, waiting: list[int]):
+        pic1 = "../before_ink.jpg"
+        pic11 = "../before_ink_bright.jpg"
+        pic2 = "../after_ink.jpg"
+        before_ink = ImageMobject(pic1)
+        after_ink = ImageMobject(pic2)
+        scale_factor = 0.5
+        before_ink.scale(scale_factor)
+        after_ink.scale(scale_factor)
+        self.add(before_ink)
         self.wait()
-
-        self.play(
-            MoveAlongPath(particle, circle),
-            MoveAlongPath(particle2, path2),
-            FadeIn(circle, rate_func=rate),
-            FadeIn(force_vector, rate_func=rate),
-            rate_func=linear,
-            run_time=t_max,
-        )
-        self.wait()
-
-        # Aufräumen
-        self.play(
-            *[
-                FadeOut(mob)
-                for mob in [
-                    B_field,
-                    # B_label,
-                    particle,
-                    # q_label,
-                    # v_label,
-                    circle,
-                    # force_vector,
-                    velocity_vector,
-                ]
-            ]
-        )
+        self.add(after_ink)
 
     def particle_deflection(self, velocity=1, field_strength=0.5, number_of_objects=1):
         # used for smoother movements
         oversampling = max(1, int(100 / config.frame_rate))
+        text = MathTex("v = ", velocity, r"\frac{m}{s}").to_edge(UP).shift(DOWN * 0.5)
         height, width = 3, 3
-        number_of_objects = 5
         field = Rectangle(height=height, width=width)
         magnet_field = VGroup(
             *[Text("×", font_size=24) for _ in range(width * height * 4)]
@@ -608,7 +428,7 @@ class LorentzKraftVideo(Scene):
                 color=RED,
             )
             # dot velocity
-            dot.v = np.array([np.random.uniform(0.5, 2), 0, 0])
+            dot.v = np.array([np.random.uniform(velocity, velocity + 0.1), 0, 0])
 
             def dotupdater(mobj, dt):
                 if dt > 0:
@@ -633,63 +453,54 @@ class LorentzKraftVideo(Scene):
             *[TracedPath(dot.get_center, stroke_color=dot.get_color()) for dot in dots]
         )
         self.add(magnet_field, field)
+        self.add(text)
         self.wait(3)
         self.add(dots, traces)
         self.wait(10)
+        self.remove(magnet_field, dots, traces, field, text)
 
+    def explain_picture(self, waiting: list[int] = [3, 3, 3]):
+        pic1 = "../before_ink.jpg"
+        pic2 = "../after_ink.jpg"
+        before_ink = ImageMobject(pic1)
+        after_ink = ImageMobject(pic2)
+        scale_factor = 1
+        before_ink.scale(scale_factor)
+        after_ink.scale(scale_factor)
 
-class Testing(ThreeDScene):
-    def construct(self):
-        # self.lorentz_particle_comparison(velocity=1.5)
-        self.particle_deflection()
+        # Define magnet center
+        magnet_center = [0, -2.5, 0]
 
-    def particle_deflection(
-        self,
-        velocity: float = 1.0,
-        field_strength: float = 0.4,
-        number_of_objects: int = 1,
-    ):
-        # used for smoother movements
-        oversampling = max(1, int(100 / config.frame_rate))
-        height, width = 3, 3
-        field = Rectangle(height=height, width=width)
-        magnet_field = VGroup(
-            *[Text("×", font_size=24) for _ in range(width * height * 4)]
-        ).arrange_in_grid(height * 2, width * 2, buff=0.35)
-        magnet_field.set_color(BLUE)
-        dots = VGroup()
-        for idx in range(number_of_objects):
-            dot = Dot(
-                point=[-3, field.get_center()[1] - 1, 0],
-                color=RED,
-            )
-            # dot velocity
-            dot.v = np.array([velocity, 0, 0])
+        arrow_length = 2
+        # Create arrows originating from magnet_center
+        b_arrow = Arrow(
+            start=magnet_center,
+            end=[magnet_center[0] + arrow_length, magnet_center[1], magnet_center[2]],
+            color=BLUE,
+        )  # Right
+        v_arrow = Arrow(
+            start=magnet_center,
+            end=[magnet_center[0], magnet_center[1] + arrow_length, magnet_center[2]],
+            color=GREEN,
+        )  # Up
+        into_arrow = Arrow(
+            start=magnet_center,
+            end=[
+                magnet_center[0] + 0.5,
+                magnet_center[1] + 0.5,
+                magnet_center[2] - arrow_length,
+            ],
+            color=RED,
+        )  # Into picture
 
-            def dotupdater(mobj, dt):
-                if dt > 0:
-                    ddt = dt / oversampling
-                    for _ in range(oversampling):
-                        p = mobj.get_center()
-                        if (field.get_left()[0] < p[0] < field.get_right()[0]) and (
-                            field.get_bottom()[1] < p[1] < field.get_top()[1]
-                        ):
-                            mobj.v += (
-                                field_strength
-                                * ddt
-                                * np.matmul(
-                                    np.array([[0, -1, 0], [1, 0, 0], [0, 0, 0]]), mobj.v
-                                )
-                            )
-                        mobj.shift(ddt * mobj.v)
+        # Add elements to the scene
+        self.add(before_ink)
+        self.add(b_arrow)  # Add arrow pointing to the right
+        self.add(v_arrow)  # Add arrow pointing upward
+        self.add(into_arrow)  # Add arrow pointing into the picture
 
-            dot.add_updater(dotupdater)
-            dots += dot
-        traces = VGroup(
-            *[TracedPath(dot.get_center, stroke_color=dot.get_color()) for dot in dots]
-        )
-        self.add(magnet_field, field)
-        self.wait(3)
-        self.add(dots, traces)
-        # self.set_camera_orientation(phi=55 * DEGREES, theta=-45 * DEGREES)
-        self.wait(10)
+        self.wait(waiting[-2])
+        self.remove(before_ink)
+        self.add(after_ink)
+        self.wait(waiting[-1])
+        self.remove(after_ink, b_arrow, v_arrow, into_arrow)
